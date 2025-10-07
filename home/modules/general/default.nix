d502a5903c919e82c5059e2644e27521ae528f0c
allomyrina volbot <volbot.tech@gmail.com>
2025-10-07 14:27:57 -0400

    add ripdrag to yazi

--- home/modules/general/default.nix
+++ home/modules/general/default.nix
@@ -1,29 +1,41 @@
 {
-  pkgs, 
+  pkgs,
   settings,
-    ...
-}: {
-  imports = if settings.hostname == "allomyrina" then [
-    ./fish
-      ./nvim
-      ./stylix.nix
-      ./starship
-  ] else [
-  ./fish
-    ./nvim
-    ./stylix.nix
-      ./starship
-  ];
-  home.packages = with pkgs; [
-    lf
+  ...
+}:
+{
+  imports =
+    if settings.hostname == "allomyrina" then
+      [
+        ./fish
+        ./nvim
+        ./stylix.nix
+        ./yazi.nix
+        ./starship
+      ]
+    else
+      [
+        ./fish
+        ./nvim
+        ./stylix.nix
+        ./yazi.nix
+        ./starship
+      ];
+  home = {
+    shell.enableFishIntegration = true;
+    packages = with pkgs; [
+      lf
       unzip
-      yazi
       lazygit
       vesktop
       fastfetch
       libsixel
 
+      yazi
+      ripdrag
+
       pnpm
       nodejs_24
-  ];
+    ];
+  };
 }
