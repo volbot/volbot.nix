373630ef3376314106c1e2512cddde4f073b9499
allomyrina volbot <tech@volbot.org>
2026-04-27 14:53:40 -0400

    add homelab setup

--- core/ssh.mod.nix
+++ core/ssh.mod.nix
@@ -1,15 +1,21 @@
 {
-  universal = {
-    services.openssh = {
-      enable = true;
-      settings = {
-        AuthenticationMethods = "publickey";
-        PermitRootLogin = "no";
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-      };
+	universal = {pkgs, ...}: {
+		services.openssh = {
+			enable = true;
+			settings = {
+				AuthenticationMethods = "publickey";
+				PermitRootLogin = "no";
+				PasswordAuthentication = false;
+				KbdInteractiveAuthentication = false;
+			};
 
-      authorizedKeysInHomedir = false;
-    };
-  };
+			authorizedKeysInHomedir = false;
+		};
+
+		programs.gnupg.agent = {
+			enable = true;
+			pinentryPackage = pkgs.pinentry-curses;
+			enableSSHSupport = true;
+		};
+	};
 }
