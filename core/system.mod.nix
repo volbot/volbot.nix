e609b80b5a2c3744a43d6f6474d6c238cceb8813
allomyrina volbot <tech@volbot.org>
2026-08-11 07:40:30 -0400

    update stateVersion and install navidrome plugins

--- core/system.mod.nix
+++ core/system.mod.nix
@@ -1,6 +1,6 @@
 {
 	universal = {pkgs, ...}: {
-		system.stateVersion = "23.11";
+		system.stateVersion = "25.11";
 		nixpkgs.config.allowUnfree = true;
 		nix.settings.experimental-features = [ "nix-command" "flakes" ];
 		environment.systemPackages = with pkgs; [
